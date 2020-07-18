263a21acd02b034ce00892a2dd51d3ca22b4ddfc
unknown <Administrator@DBH-NYAMJANTSAN.statebank.mn>
2020-07-18 21:26:21 +0800

    init

---
+++ src/main/java/com/lucierp/LucierpApplication.java
@@ -0,0 +1,13 @@
+package com.lucierp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucierpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucierpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucierp/LucierpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucierpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
